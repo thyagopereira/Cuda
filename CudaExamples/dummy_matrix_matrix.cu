eec5e29e54601ebb5cb8481534d734ba7f735279
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2024-05-10 21:26:17 -0300

    Multiplicação de matrizes em progresso

---
+++ CudaExamples/dummy_matrix_matrix.cu
@@ -0,0 +1,83 @@
+#include "cuda_runtime.h"
+#include <cuda.h>
+#include <stdio.h>
+#include<unistd.h>
+
+
+// O ponto desse exercicio é forçar a manipulação de um problema utilizando duas
+// duas dimensões do grid; 
+// Lembre-se que o numero de threads, deve ser sempre uma constante multipla de 32
+// aredonda-da para cima via ceil, de modo a evitar indices não mapeados.
+
+const int N = 32 ;// Numero de linhas da matriz
+const int M = 32 ; // Numero de colunas da matriz
+
+
+// R = M X N 
+__global__ void multiplica(float* m, float* n, float* r, int width) {
+
+
+    printf("CUDA IS RUNNING"); //  Nao printa pq ? --- tem tempo sleep(20);
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+    int col = blockIdx.x*blockDim.x + threadIdx.x;
+
+    printf("%d, %d", row, col);
+
+    // Isso torna possível trabalhar apenas matrizes quadradas? 
+    if((row < width) && (col <  width)){
+        float Pvalue = 0;
+
+        for(int k = 0; k < width; k++){
+            Pvalue += m[row*width + k] * n[k*width+col];
+        }
+
+        r[row*width + col] = Pvalue;
+        printf("%f \n", r[row*width + col]);
+    }
+}
+
+int main() {
+
+    cudaDeviceReset();
+    float *dma, *dmb, *dmc;
+    float *ma, *mb, *mc;
+    int size = N * M * sizeof(float); // Size of a NxM matriz
+
+
+    // Declarando matriz in memory do host -- Isso me permite setar valores nela  
+    ma = (float*)malloc(size);
+    mb = (float*)malloc(size);
+    mc = (float*)malloc(size);
+
+    // Declarando matrizes na memoria do device -- APENAS DECLARADO 
+    cudaMalloc((void**)&dma, size); 
+    cudaMalloc((void**)&dmb, size);
+    cudaMalloc((void**)&dmc, size);
+
+    // Preenchendo matriz com valores  
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++){
+            ma[i * M + j] = i + j;
+            mb[i * M + j] = i + j;
+        }
+    }
+
+    cudaMemcpy(dma, ma, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dmb, mb, size, cudaMemcpyHostToDevice);
+
+    multiplica<<<32, 32>>>(dma, dmb, dmc, N);
+
+
+    cudaMemcpy(mc, dmc, size, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    cudaFree(dma); cudaFree(dmb); cudaFree(dmc);
+
+    // for(int i = 0; i < N; i++) {
+    //     for(int j = 0; j < M; j++){
+    //         printf("R[%d][%d] = %f \n", i,j, mc[i * M + j]);
+    //     }
+    // }
+    
+    sleep(20);
+    return 0;
+}
