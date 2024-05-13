f0c87b8088ee19561d1a12aa31cb09cade5dd4da
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2024-05-13 14:49:21 -0300

    dummy matrix matrix failed, try again

--- CudaExamples/dummy_matrix_matrix_failed.cu
+++ CudaExamples/dummy_matrix_matrix_failed.cu
@@ -8,28 +8,36 @@
 // Lembre-se que o numero de threads, deve ser sempre uma constante multipla de 32
 // aredonda-da para cima via ceil, de modo a evitar indices não mapeados.
 
-const int N = 32 ;// Numero de linhas da matriz
-const int M = 32 ; // Numero de colunas da matriz
+
+// Usando sempre matriz quadradas para evitar mapear numero de colunas e de linhas 
+// de cada matriz. 
+const int N = 2 ;// Numero de linhas da matriz
+const int M = 2 ; // Numero de colunas da matriz
 
 
 // R = M X N 
 __global__ void multiplica(float* ma, float* mb, float* mc, int width) {
 
-    int row = blockIdx.y*blockDim.y + threadIdx.y;
-    int col = blockIdx.x*blockDim.x + threadIdx.x;
-
-    // Isso torna possível trabalhar apenas matrizes quadradas? 
-    if((row < width) && (col <  width)){
-        float Pvalue = 0;
-
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    // Isso torna possível trabalhar apenas matrizes quadradas N x N 
+    if(row < 2 && col < 2){
+        // Numero de colunas de Ma deve ser igual o numero de colunas de MB (4x4)
+        // Multiplicação de vetores.
+        float value = 0;
         for(int k = 0; k < width; k++){
-            Pvalue += ma[row*width + k] * mb[k*width+col];
+            value += ma[row * width + k] * mb[k * width + col];
         }
 
-        mc[row*width + col] = Pvalue;
+        printf("Value %f \n", value);
+        mc[row*width + col] = value;
+        printf("mc[%d] \n", row*width + col);
     }
 }
 
+
+// Só vai entender no papel.
 int main() {
 
     cudaDeviceReset();
@@ -51,19 +59,34 @@ int main() {
     // Preenchendo matriz com valores  
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++){
-            ma[i * M + j] = i + j;
-            mb[i * M + j] = i + j;
+            ma[i * M + j] = 2;
+            mb[i * M + j] = 2;
         }
     }
+
+    // printf("Inited matrix A \n");
+    // for(int i = 0; i < N; i++) {
+    //     for(int j = 0; j < M; j++){
+    //         printf("R[%d][%d] = %f \n", i,j, ma[i * M + j]);
+    //     }
+    // }
+
+    // printf("Inited matrix B \n");
+    // for(int i = 0; i < N; i++) {
+    //     for(int j = 0; j < M; j++){
+    //         printf("R[%d][%d] = %f \n", i,j, mb[i * M + j]);
+    //     }
+    // }
 
     cudaMemcpy(dma, ma, size, cudaMemcpyHostToDevice);
     cudaMemcpy(dmb, mb, size, cudaMemcpyHostToDevice);
 
-    multiplica<<<32, 32>>>(dma, dmb, dmc, N);
-    cudaDeviceSynchronize();
+    multiplica<<<2,2>>>(dma, dmb, dmc, N*M);
+    cudaDeviceSynchronize(); // Always before cudaMemcpy  device -> host (u can get crap if not this way)
     cudaMemcpy(mc, dmc, size, cudaMemcpyDeviceToHost);
     
 
+    // Cuda kernels does not throw exceptions, ALWAYS CHECK FOR ERROR
     cudaError_t error = cudaGetLastError();
     if(error != cudaSuccess)
     {
@@ -74,6 +97,7 @@ int main() {
 
     cudaFree(dma); cudaFree(dmb); cudaFree(dmc);
 
+    printf("Result Matrix C \n");    
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++){
             printf("R[%d][%d] = %f \n", i,j, mc[i * M + j]);
